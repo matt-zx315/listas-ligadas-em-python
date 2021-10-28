dabd459c5a2acd2e26d4f5bd40897339f2b25fc8
matt-zx315 <31132418+matt-zx315@users.noreply.github.com>
2021-10-28 14:57:55 -0300

    Invertendo a ordem de uma lista

---
+++ Lista_Ligada_simples_21.py
@@ -0,0 +1,70 @@
+# Invertendo uma lista
+class Node:
+    
+    
+    def __init__(self, _data):
+        self.data = _data
+        self.next = None
+
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+    
+    
+    def append(self, _new_data):
+        new_node = Node(_new_data)
+        
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        
+        while(last.next):
+            last = last.next
+        
+        last.next = new_node
+    
+
+    def print_list(self):
+        if self.head is None:
+            print("Vazio... Igualzinho tua cabeça!!!")
+            return
+        
+        temp = self.head
+        list_data = ""
+        
+        while(temp):
+            list_data += str(temp.data) + " "
+            temp = temp.next
+        
+        print(list_data)
+    
+    
+    def reverse_list(self):
+        prev_node, next_node = None, None
+        curr_node = self.head
+        
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        
+        self.head = prev_node
+
+
+list1 = LinkedList()
+list1.append(65)
+list1.append(71)
+list1.append(26)
+list1.append(84)
+list1.append(12)
+list1.append(90)
+list1.append(36)
+list1.print_list()
+
+list1.reverse_list()
+list1.print_list()
